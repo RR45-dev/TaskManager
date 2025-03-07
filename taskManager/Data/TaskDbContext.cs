ba9fb64e3c7377c1d7f81a3cda8e4d829a904ae7
RR45-dev <id>
2025-03-06 22:07:12 -0700

    update

--- taskManager/Data/TaskDbContext.cs
+++ taskManager/Data/TaskDbContext.cs
@@ -5,7 +5,9 @@ using taskManager.Models;
 
 public class TaskDbContext : DbContext
 
-    /*The TaskDbContext class will act as a bridge between your C# application and the database, mapping your C# models to database tables.*/
+/*The TaskDbContext class will act as a bridge between your C# application and the database, mapping your C# models to database tables.*/
+/*The TaskDbContext class will act as a bridge between your C# application and the database, mapping your C# models to database tables.*/
+
 {
     public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options) { }
     public DbSet<TaskItems> TaskTable { get; set; }
